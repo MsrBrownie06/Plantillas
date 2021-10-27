d623cd28588f63baf57af1e9117cd92131cd8b6e
Braian <braianoscar.loza@tajamar365.com>
2021-10-27 14:23:26 +0200

    27 de octubre

--- REACT/reactdoctores/src/Components/Router/Router.js
+++ REACT/reactdoctores/src/Components/Router/Router.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import InsertarDoctor from "../Doctores/InsertarDoctor";
+import MostrarDoctores from "../Doctores/MostrarDoctores";
 export default class Router extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route />
+          <Route to="/mostrar" component={MostrarDoctores} />
+          <Route to="/insertar" component={InsertarDoctor} />
         </Switch>
       </BrowserRouter>
     );
