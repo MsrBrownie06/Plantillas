2f88e2d90f8fe547fea984e9b3f3530966e1437d
MRSBrownie06 <braianloza06@gmail.com>
2021-11-13 22:28:37 +0100

    13 de noviembre

--- POST/reactanimacionsonidos/src/Components/Navegacion/Elemento.js
+++ POST/reactanimacionsonidos/src/Components/Navegacion/Elemento.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
-import Genshin from "genshin"
+
 
 export default class Elemento extends Component {
 
   state = {
-    nombres: ["amber", "ayaka", "baizhu", "barbara", "beidou", "bennett", "childe", "chongyun", "cyno", "diluc", "diona", "fischl", "ganyu", "jean", "kaeya", "keqing", "klee", "lisa", "mona", "ningguang", "noelle", "qiqi", "razor", "sucrose", "venti", "xiangling", "xiao", "xingqiu", "xinyan", "zhongli"],
+    nombres: ["amber", "ayaka", "baizhu", "barbara", "beidou", "bennett", "childe", "chongyun", "diluc", "diona", "fischl", "ganyu", "jean", "kaeya", "keqing", "klee", "lisa", "mona", "ningguang", "noelle", "qiqi", "razor", "sucrose", "venti", "xiangling", "xiao", "xingqiu", "xinyan", "zhongli"],
     personajes: [],
     estado: false
   }
 
   cargarPersonajes() {
     for (let i = 0; i < this.state.nombres.length; i++) {
-      Genshin.characters(this.state.nombres[i]).then(res => {
-        let personaje = {
-          nombre: res.name,
-          imagen: res.image,
-          elemento: res.element,
-          ciudad: res.city,
-          arma: res.weapon,
-          descripcion: res.description
-        }
+      // Genshin.characters(this.state.nombres[i]).then(res => {
+      //   let personaje = {
+      //     nombre: res.name,
+      //     imagen: res.image,
+      //     elemento: res.element,
+      //     ciudad: res.city,
+      //     arma: res.weapon,
+      //     estrellas: res.rating
+      //   }
 
-        this.state.personajes.push(personaje)
-      })
+      //   this.state.personajes.push(personaje)
+      // })
     }
     this.setState({
       estado: true
@@ -30,27 +30,32 @@ export default class Elemento extends Component {
   }
 
   componentDidMount = () => {
-    this.cargarPersonajes()
+    //this.cargarPersonajes()
   };
 
   render() {
     if (this.state.estado === true) {
       return (
-        <div>
-          <div className="card mb-3 m-2" style={{ maxWidth: "540px" }}>
-            <div className="row g-0">
-              <div className="col-md-4">
-                <img src={""} className="img-fluid rounded-start" alt="nombreImagen" />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Titulo de la Carta</h5>
-                  <p className="card-text">Descripcion y texto</p>
-                  <p className="card-text"><small className="text-muted">Texto en pequeño</small></p>
+        <div className="row row-cols-4 g-2">
+          {this.state.personajes.map((personaje, index) => {
+            if (personaje.elemento.toLowerCase() === this.props.vision) {
+              return (
+                <div className="col text-center">
+                  <div key={index} className="card mb-3 m-2" style={{ maxWidth: "350px" }}>
+
+                    <img src={personaje.imagen} className="img-fluid card-img-top" alt="nombreImagen" />
+                    <div className="card-body">
+                      <h1 className="card-title">{personaje.nombre}</h1>
+                      <p className="card-text">Weapon: <b>{personaje.arma}</b></p>
+                      <p className="card-text"><small className="text-muted">Rarity: {personaje.estrellas} stars</small></p>
+                    </div>
+
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
+
+              )
+            }
+          })}
         </div >
       );
     } else {
