6a4277c4de415ac7ade9532e0930f2ff35ba931a
MRSBrownie06 <braianloza06@gmail.com>
2021-11-16 19:00:23 +0100

    16 de noviembre

---
+++ VUE/futbolvue/src/components/Global.js
@@ -0,0 +1,6 @@
+var Global = {
+
+    urlFutbol: "https://apichampions.azurewebsites.net"
+}
+
+export default Global;
---
+++ VUE/futbolvue/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import App from './App.vue'
+
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+
+const routes = [
+
+]
+
+const router = new VueRouter({
+  routes,
+  mode: "history"
+})
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
