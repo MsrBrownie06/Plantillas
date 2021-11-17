b4f80c4aebd6f53f8401083741a922ceb2df358e
Braian <braianoscar.loza@tajamar365.com>
2021-11-17 14:22:44 +0100

    17 de noviembre

--- ANGULAR/angularcruddepartamentos/src/app/Components/departamentos/departamentos.component.ts
+++ ANGULAR/angularcruddepartamentos/src/app/Components/departamentos/departamentos.component.ts
@@ -12,11 +12,13 @@ import { Departamento } from 'src/app/Models/Departamento';
 export class DepartamentosComponent implements OnInit {
 
   public departamentos!: Array<Departamento>
+  public id!: string
 
   constructor(private _service: DepartamentoService, private _param: ActivatedRoute, private _router:Router) { }
 
   ngOnInit(): void {
     this.cargarDepartamentos()
+    //Forma 1
     this._param.params.subscribe((params: Params)=>{
       if(params["idDept"] != null){
         this._service.deleteDepartamento(params["idDept"]).subscribe(res=>{
@@ -25,7 +27,13 @@ export class DepartamentosComponent implements OnInit {
       }
     })
   }
-  
+  //Forma 2
+  eliminarDepartamento(){
+    this._service.deleteDepartamento(this.id).subscribe(res=>{
+      
+    })
+  }
+
   cargarDepartamentos(){
     this._service.getDepartamentos().subscribe(res=>{
       this.departamentos = res
